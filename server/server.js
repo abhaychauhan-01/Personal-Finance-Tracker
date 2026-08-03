34c76d0bf816e5c8f4d9fe38cd24fa6d360341f2
ABHAY PRATAP CHAUHAN <abhaychauhan2906@gmail.com>
2026-08-03 13:01:58 +0530

    checking

--- server/server.js
+++ server/server.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const budgetRoutes =
-require("./routes/budgetRoutes");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(
-  "/api/budget",
-  budgetRoutes
-);
 
-const authRoutes = require('./routes/authRoutes');
-const transactionRoutes = require('./routes/transactionRoutes');
+const authRoutes = require("./routes/authRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+const budgetRoutes = require("./routes/budgetRoutes");
 
-app.use('/api/auth', authRoutes);
-app.use('/api/transactions', transactionRoutes);
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch((err) => console.log(err));
+app.use("/api/auth", authRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/budget", budgetRoutes);
 
 const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection Failed");
+    console.error(err);
+  });
